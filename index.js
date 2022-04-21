3a231335da171b9d475da533579f2b91b490e675
Parnell <psteide@ford.com>
2022-04-21 08:53:44 -0400

    add begining js to index page

--- index.js
+++ index.js
@@ -1,18 +1,33 @@
-const form = document.querySelector("form")
+const form = document.querySelector("form");
+const category = form.querySelector("#item-category");
+const p = form.querySelector("p");
+
+category.addEventListener("change", (event) => {
+  const selector = form.querySelector("#item-category");
+  const itemCategory = selector.value;
+  const qualityInput = form.querySelector("#quality");
+  if (itemCategory === "Sulfuras-Hand-of-Ragnaros") {
+    qualityInput.value = 80;
+  } else {
+    qualityInput.max = 50;
+  }
+});
 
 form.addEventListener("submit", (event) => {
-event.preventDefault()
-const formData = new FormData(event.target)
-const itemName = formdata.get("item-name")
-const itemCategory = formdata.get("item-category")
-const sellInDays = formdata.get("sell-in")
-const quality = formdata.get("quality")
-const dateAdded = formdata.get("date")  
+  event.preventDefault();
+  const formData = new FormData(event.target);
+  const itemName = formData.get("item-name");
+  const itemCategory = formData.get("item-category");
+  const sellInDays = formData.get("sell-in");
+  const quality = formData.get("quality");
+  const dateAdded = formData.get("date");
+  const item = {
+    itemName,
+    itemCategory,
+    sellInDays,
+    quality,
+    dateAdded,
+  };
 
-
-}
-
-
-
-
-)
+  console.log(item);
+});
